fad292c2afccf9fe53a1f3a6f971d881fcf0ebc1
dvoth <dvoth@vervocity.io>
2018-02-15 22:58:09 -0600

    Added ability for device to receive json data from web api

---
+++ app/src/main/java/edu/siue/accountingbootcamp/services/MyService.java
@@ -0,0 +1,55 @@
+package edu.siue.accountingbootcamp.services;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import java.io.IOException;
+
+import edu.siue.accountingbootcamp.utils.HttpHelper;
+
+public class MyService extends IntentService {
+
+    public static final String TAG = "MyService";
+    public static final String MY_SERVICE_MESSAGE = "myServiceMessage";
+    public static final String MY_SERVICE_PAYLOAD = "myServicePayload";
+
+    public MyService() {
+        super("MyService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Uri uri = intent.getData();
+        Log.i(TAG, "testerton: " + uri.toString());
+
+        String response;
+        try {
+            response = HttpHelper.downloadUrl(uri.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Intent messageIntent = new Intent(MY_SERVICE_MESSAGE);
+        messageIntent.putExtra(MY_SERVICE_PAYLOAD, response);
+        LocalBroadcastManager manager =
+                LocalBroadcastManager.getInstance(getApplicationContext());
+        manager.sendBroadcast(messageIntent);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(TAG, "onCreate");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
+
+}
