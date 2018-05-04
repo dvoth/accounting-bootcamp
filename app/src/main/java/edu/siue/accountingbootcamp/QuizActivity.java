c8f617dfce3c10ef243fba1d3401b9e5e1306fff
dvoth <dvoth@vervocity.io>
2018-05-03 19:42:09 -0500

    Initial quiz gui interface

---
+++ app/src/main/java/edu/siue/accountingbootcamp/QuizActivity.java
@@ -0,0 +1,39 @@
+package edu.siue.accountingbootcamp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import edu.siue.accountingbootcamp.models.Quiz;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        // Get the intent (containing quiz data) from QuizListAdapter
+        Intent intent = getIntent();
+        Quiz quiz = intent.getParcelableExtra(QuizListAdapter.ITEM_KEY);
+
+        // Add toolbar and update default text to quiz name
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(quiz.getName());
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
--- app/src/main/java/edu/siue/accountingbootcamp/QuizListAdapter.java
+++ app/src/main/java/edu/siue/accountingbootcamp/QuizListAdapter.java
@@ -1,6 +1,7 @@
 package edu.siue.accountingbootcamp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.widget.RecyclerView;
@@ -9,7 +10,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.List;
 
@@ -59,21 +59,10 @@ public class QuizListAdapter extends RecyclerView.Adapter<QuizListAdapter.ViewHo
         holder.mView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(mContext, "You selected " + quiz.getName(),
-                        Toast.LENGTH_SHORT).show();
-//                String itemId = quiz.getItemId();
-//                Intent intent = new Intent(mContext, DetailActivity.class);
-//                intent.putExtra(ITEM_KEY, quiz);
-//                mContext.startActivity(intent);
-            }
-        });
-
-        holder.mView.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                Toast.makeText(mContext, "You long clicked ",
-                        Toast.LENGTH_SHORT).show();
-                return false;
+                int itemId = quiz.getId();
+                Intent intent = new Intent(mContext, QuizActivity.class);
+                intent.putExtra(ITEM_KEY, quiz);
+                mContext.startActivity(intent);
             }
         });
     }
